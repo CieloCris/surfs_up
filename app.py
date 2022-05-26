a61189647ba949c48826be6136d7fa302f7c4c23
CieloCris <97429651+CieloCris@users.noreply.github.com>
2022-05-26 15:40:03 -0500

    app.py file modifications

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+# Import the dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
 
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+# Dependencies we need for SQLAlchemy
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# Dependencies for Flask
+from flask import Flask, jsonify
+
+#from flask import Flask
+
+#app = Flask(__name__)
+#@app.route('/')
+#def hello_world():
+#    return 'Hello world'
