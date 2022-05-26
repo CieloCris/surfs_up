1bf5d3a0af64a45b764ac66ed5801f98d829f3b1
CieloCris <97429651+CieloCris@users.noreply.github.com>
2022-05-26 15:50:24 -0500

    modifications to files

--- app.py
+++ app.py
@@ -1,20 +1,49 @@
-# Import the dependencies
+# Set up the Flask Weather App
+# Import dependencies
 import datetime as dt
 import numpy as np
 import pandas as pd
 
-# Dependencies we need for SQLAlchemy
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-# Dependencies for Flask
 from flask import Flask, jsonify
 
-#from flask import Flask
+# Set up database engine for Flask app
+# Create function allows access to SQLite database file
+engine = create_engine("sqlite:///hawaii.sqlite")
+# Reflect database into classes
+Base = automap_base()
+# Reflect tables
+Base.prepare(engine, reflect=True)
+# Set class variables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+# Creates session link from Python to SQLite database
+session = Session(engine)
+# Create Flask app, all routes go after this code
+app = Flask(__name__)
 
-#app = Flask(__name__)
-#@app.route('/')
-#def hello_world():
-#    return 'Hello world'
+# Example of app name variable
+# import app
+# print("example __name__ = %s", __name__)
+# if __name__ == "__main__":
+#     print("example is being run directly.")
+# else:
+#     print("example is being imported")
+
+# Define welcome route
+@app.route("/")
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''') 
